2c13b379201c734c2e2884eef0885e7156b696ba
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-01-08 03:44:53 +0100

    React with colour emoji instead of a message with image attachments

--- bin/discord_colour_mention_bot.dart
+++ bin/discord_colour_mention_bot.dart
@@ -1,16 +1,9 @@
+import "dart:async";
 import "dart:io";
-import "dart:typed_data";
 
-import "package:image/image.dart" as img;
 import "package:nyxx/nyxx.dart";
 
-final img.BitmapFont font = img.readFontZip(File("JBMono.zip").readAsBytesSync());
-//the font is actually 40, but we add a little more for the extra spacing between the lines
-const int textHeight = 48;
-
-const int imageSize = 512;
-
-const String deleteEmoji = "❌";
+import "emoji_cache.dart";
 
 final RegExp hexColourRegex = RegExp("#[0-9a-fA-F]{6,8}");
 
@@ -24,8 +17,18 @@ Future<void> main(List<String> arguments) async {
   final NyxxGateway client = await Nyxx.connectGateway(
     token,
     GatewayIntents.allUnprivileged | GatewayIntents.messageContent,
-    options: GatewayClientOptions(plugins: <NyxxPlugin<Nyxx>>[logging, cliIntegration]),
+    options: GatewayClientOptions(
+      plugins: <NyxxPlugin<Nyxx>>[
+        logging,
+        cliIntegration,
+        ignoreExceptions,
+      ],
+      emojiCacheConfig: const .new(maxSize: cacheSizeLimit),
+    ),
   );
+
+  final EmojiCache emojiCache = EmojiCache(client);
+  await emojiCache.init();
 
   final User botUser = await client.users.fetchCurrentUser();
 
@@ -45,103 +48,19 @@ Future<void> main(List<String> arguments) async {
         .whereType<String>()
         .toSet();
 
-    final List<AttachmentBuilder> attachments = <AttachmentBuilder>[];
-    for (final String colour in colours.take(9)) {
-      final Uint8List? imageData = await generateImageForColour(colour);
-      if (imageData == null) continue;
-      final AttachmentBuilder attachment = AttachmentBuilder(
-        data: imageData,
-        fileName: "$colour.png",
-      );
-      attachments.add(attachment);
+    //20 is the maximum amount of reactions
+    for (final String colour in colours.take(20)) {
+      final Emoji? colourEmoji = await emojiCache.getEmojiForColour(colour);
+      if (colourEmoji == null) continue;
+      await event.message.react(ReactionBuilder.fromEmoji(colourEmoji));
     }
-
-    //don't reply if there ended up being no attachments generates
-    if (attachments.isEmpty) return;
-
-    final Message sentMessage = await event.message.channel.sendMessage(
-      MessageBuilder(
-        content:
-            "This is what${colours.length > 9 ? " (the first nine of)" : ""} ${attachments.length > 1 ? "those colours" : "that colour"} look${attachments.length > 1 ? "" : "s"} like:",
-        referencedMessage: MessageReferenceBuilder.reply(messageId: event.message.id),
-        flags: MessageFlags.suppressNotifications,
-        //no message sound (if you're in the channel)
-        allowedMentions: AllowedMentions(),
-        //no mention (ping)
-        attachments: attachments,
-      ),
-    );
-
-    await sentMessage.react(ReactionBuilder(name: deleteEmoji, id: null));
   });
 
-  client.onMessageReactionAdd.listen((MessageReactionAddEvent event) async {
-    //only delete on delete emoji
-    if (event.emoji.name != deleteEmoji) return;
+  //click the reaction to remove it
+  client.onMessageReactionAdd.listen((MessageReactionAddEvent event) {
+    //check if message author reacted
+    if (event.userId != event.messageAuthorId) return;
 
-    //only delete own messages
-    final Message colourBotMessage = await event.message.get();
-    if (colourBotMessage.author.id != botUser.id) return;
-
-    //not a reply
-    final MessageReference? replyTo = colourBotMessage.reference;
-    if (replyTo == null) return;
-    final PartialMessage? replyMessage = replyTo.message;
-    if (replyMessage == null) return;
-
-    //only allow the original author delete the bot message
-    final Message originalMessage = await replyMessage.get();
-    if (event.userId != originalMessage.author.id) return;
-
-    await event.message.delete();
+    unawaited(event.message.deleteReaction(ReactionBuilder.fromEmoji(event.emoji)));
   });
-}
-
-Future<Uint8List?> generateImageForColour(String hexString) async {
-  final int? ox = int.tryParse(hexString.replaceFirst("#", "0x"));
-  if (ox == null) return null;
-
-  final img.Color colour;
-  final String rgbString;
-  if (hexString.length == 1 + 6) {
-    final int b = ox & 255;
-    final int g = (ox >> 8) & 255;
-    final int r = (ox >> 16) & 255;
-    colour = img.ColorRgb8(r, g, b);
-    rgbString = "rgb($r,$g,$b)";
-  } else if (hexString.length == 1 + 8) {
-    final int a = ox & 255;
-    final int b = (ox >> 8) & 255;
-    final int g = (ox >> 16) & 255;
-    final int r = (ox >> 24) & 255;
-    colour = img.ColorRgba8(r, g, b, a);
-    rgbString = "rgba($r,$g,$b,$a)";
-  } else {
-    return null;
-  }
-
-  final double luminance =
-      (0.299 * colour.r + 0.587 * colour.g + 0.114 * colour.b) / 255;
-  final img.Color textColour = luminance > 0.5
-      ? img.ColorRgb8(0, 0, 0)
-      : img.ColorRgb8(255, 255, 255);
-
-  final img.Command command = img.Command()
-    ..createImage(width: imageSize, height: imageSize, numChannels: 4)
-    ..fill(color: colour)
-    ..drawString(
-      hexString,
-      font: font,
-      color: textColour,
-      y: imageSize ~/ 2 - textHeight,
-    )
-    ..drawString(
-      rgbString,
-      font: font,
-      color: textColour,
-      y: imageSize ~/ 2,
-    )
-    ..encodePng();
-
-  return command.getBytes();
 }
---
+++ bin/emoji_cache.dart
@@ -0,0 +1,96 @@
+import "dart:typed_data";
+
+import "package:image/image.dart" as img;
+import "package:nyxx/nyxx.dart";
+import "package:nyxx/src/models/emoji.dart";
+
+//could technically be 2000, but added a 50 buffer Just In Case™
+const int cacheSizeLimit = 1950;
+
+class EmojiCache {
+  final NyxxGateway _client;
+
+  final Map<String, ApplicationEmoji> _emojiDict = <String, ApplicationEmoji>{};
+
+  EmojiCache(NyxxGateway client) : _client = client;
+
+  Future<void> init() async {
+    final List<ApplicationEmoji> emojiList = await _client.application.emojis.list();
+    for (final ApplicationEmoji emoji in emojiList) {
+      _emojiDict[emoji.name] = emoji;
+    }
+  }
+
+  String hexColourToKey(String hexColour) {
+    final String key = hexColour.replaceFirst("#", "").toUpperCase();
+
+    //if the provided alpha is 255, we omit it from the key
+    if (key.length == 8 && key.endsWith("FF")) {
+      return key.substring(0, key.length - 2);
+    }
+
+    return key;
+  }
+
+  Future<Emoji?> getEmojiForColour(String hexColour) async {
+    final String key = hexColourToKey(hexColour);
+
+    //check if we have this colour emoji already
+    if (_emojiDict.containsKey(key)) {
+      return _emojiDict[key];
+    }
+
+    //we didn't have this colour emoji yet, so we make it
+    final Uint8List? imageData = await generateImageForColour(hexColour);
+    if (imageData == null) return null;
+
+    final Cache<Emoji> internalCache = _client.application.emojis.cache;
+    if (internalCache.length >= cacheSizeLimit) {
+      //delete an old emoji to make space for this one
+      //TODO: can we make it delete the least-often used emoji..?
+      final List<Snowflake> list = internalCache.keys.toList(growable: false)
+        ..sort((Snowflake a, Snowflake b) => a.value.compareTo(b.value));
+      await _client.application.emojis.delete(list.first);
+    }
+
+    final ApplicationEmoji newEmoji = await _client.application.emojis.create(
+      ApplicationEmojiBuilder(
+        name: key,
+        image: ImageBuilder.png(imageData),
+      ),
+    );
+
+    return _emojiDict[key] = newEmoji;
+  }
+}
+
+const int imageSize =
+    128; //that's the size discord wants, but 1×1 technically works too...
+
+Future<Uint8List?> generateImageForColour(String hexString) async {
+  final int? ox = int.tryParse(hexString.replaceFirst("#", "0x"));
+  if (ox == null) return null;
+
+  final img.Color colour;
+  if (hexString.length == 1 + 6) {
+    final int b = ox & 255;
+    final int g = (ox >> 8) & 255;
+    final int r = (ox >> 16) & 255;
+    colour = img.ColorRgb8(r, g, b);
+  } else if (hexString.length == 1 + 8) {
+    final int a = ox & 255;
+    final int b = (ox >> 8) & 255;
+    final int g = (ox >> 16) & 255;
+    final int r = (ox >> 24) & 255;
+    colour = img.ColorRgba8(r, g, b, a);
+  } else {
+    return null;
+  }
+
+  final img.Command command = img.Command()
+    ..createImage(width: imageSize, height: imageSize, numChannels: 4)
+    ..fill(color: colour)
+    ..encodePng();
+
+  return command.getBytes();
+}
